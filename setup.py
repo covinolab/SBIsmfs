7fc7d711b923af83571392432b7eda84005317a3
Dingel321 <Dingel321@gmail.com>
2023-03-19 22:52:40 +0100

    added new setup, renamed scr to src

--- setup.py
+++ setup.py
@@ -2,27 +2,34 @@ from setuptools import Extension, setup
 from Cython.Build import cythonize
 import numpy as np
 
+path_to_gsl_include = 'path/to/gsl/include'
+path_to_gsl_lib = 'path/to/gsl/lib'
+
 extensions = [
     Extension(
         'brownian_integrator',
-        ['scr/simulator/brownian_integrator.pyx'],
-        include_dirs=[np.get_include()],
-        libraries=['gsl']
+        ['brownian_integrator.pyx'],
+        include_dirs=[np.get_include(), path_to_gsl_include],
+        library_dirs=[path_to_gsl_lib],
+        libraries=['gsl', 'gslcblas']
     ),
     Extension(
         'gls_random_gen',
-        ['scr/utils/gls_random_gen.pyx'],
-        include_dirs=[np.get_include()],
-        libraries=['gsl']
+        ['gls_random_gen.pyx'],
+        include_dirs=[np.get_include(), path_to_gsl_include],
+        library_dirs=[path_to_gsl_lib],
+        libraries=['gsl', 'gslcblas']
     ),
     Extension(
         'gls_spline',
-        ['scr/utils/gls_spline.pyx'],
-        include_dirs=[np.get_include()],
-        libraries=['gsl']
+        ['gls_spline.pyx'],
+        include_dirs=[np.get_include(), path_to_gsl_include],
+        library_dirs=[path_to_gsl_lib],
+        libraries=['gsl', 'gslcblas']
     )
 ]
 
 setup(
     ext_modules=cythonize(extensions),
+    compiler_directives={'language_level' : "3"}
 )
