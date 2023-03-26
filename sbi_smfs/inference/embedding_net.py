817899a795dfbab8479217a28edfb2db803fe687
Dingel321 <Dingel321@gmail.com>
2023-03-26 17:57:30 +0200

    reformatted embedding net file

--- sbi_smfs/inference/embedding_net.py
+++ sbi_smfs/inference/embedding_net.py
@@ -5,23 +5,24 @@ import torch.nn.functional as F
 
 class SimpleCNN(nn.Module):
     """Simple single layer CNN with ReLU activation."""
+
     def __init__(
-            self,
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            num_bins,
-            num_lags,
-            activation=nn.ReLU
-        ):
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride,
+        num_bins,
+        num_lags,
+        activation=nn.ReLU,
+    ):
         super(SimpleCNN, self).__init__()
 
         self.conv1 = nn.Conv2d(
             in_channels=in_channels,
             out_channels=out_channels,
             kernel_size=kernel_size,
-            stride=stride
+            stride=stride,
         )
         self.num_bins = num_bins
         self.num_lags = num_lags
@@ -29,8 +30,6 @@ class SimpleCNN(nn.Module):
 
     def forward(self, x):
         # Check the original matrix size and lag times!!!
-        x = x.view(
-            -1, self.num_lags, self.num_bins, self.num_bins
-        )
+        x = x.view(-1, self.num_lags, self.num_bins, self.num_bins)
         x = self.activation(self.conv1(x))
         return x.flatten(start_dim=1)
