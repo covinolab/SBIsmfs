5fc57278064922dc2b9c6666d5f7397270e45420
Dingel321 <Dingel321@gmail.com>
2023-03-26 18:47:30 +0200

    fixed test failure

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -8,7 +8,7 @@ def test_simple_cnn():
     batch_size = 19
 
     data = torch.randn((batch_size, num_lags, num_bins, num_bins)).flatten(start_dim=1)
-    cnn = SimpleCNN(num_lags, num_lags, 4, 2, num_bins, num_lags)
+    cnn = SimpleCNN(num_lags, 4, 2, num_bins, num_lags)
     out = cnn(data)
 
     assert out.shape[0] == batch_size
