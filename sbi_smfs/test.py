d20bbf35dcbc371c9061757e53a883414b9afe8b
Dingel321 <Dingel321@gmail.com>
2023-03-26 14:35:18 +0200

    deleted unused files

--- sbi_smfs/test.py
+++
@@ -1,6 +0,0 @@
-from simulator.simulator import get_simulator_from_config
-import torch
-import matplotlib.pyplot as plt
-q = get_simulator_from_config('test.config')(torch.ones(13))
-plt.plot(q)
-plt.show()
